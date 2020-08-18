1c1e5441ecc27a1674daab60c981703c7f9ee4a5
_sandarbhTiwari <tiwari.sandarbh2603@gmail.com>
2020-08-18 16:42:44 +0530

    Initial Commit. Tutorial 1.

---
+++ create_db_table.sql
@@ -0,0 +1,6 @@
+# CREATE DATABASE classroom;    #sql statement to create database named as classroom!
+# USE classroom;   #statement to use the created database
+
+# -----------------------------
+# sql statement to create a table (in selected database) having specifications as shown:
+# CREATE TABLE scienceClass(enrollment_num INT, studName VARCHAR(25), scienceMarks INT);
