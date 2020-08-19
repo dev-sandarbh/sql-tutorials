2376e9e7e0def8a5562e3d165d3e3aac276dde88
_sandarbhTiwari <tiwari.sandarbh2603@gmail.com>
2020-08-19 19:13:34 +0530

    sql filtering commands added.

---
+++ filtering_commands.sql
@@ -0,0 +1,11 @@
+USE classroom; 
+SELECT * FROM scienceClass;
+-- Adding 15 more records to get some more data to operate on
+-- INSERT INTO scienceClass VALUES 
+-- (16,'Rambo',39),(17,'Neno',89),(18,'Pinky',32),
+-- (19,'Joe',75),(20,'Seenu',65),(21,'Sino',45),
+-- (22,'Pradeep',99),(23,'Sandeep',55),(24,'Kumar',87),
+-- (25,'Shiny',99),(26,'Sakhi',87),(27,'Shivam',77),
+-- (28,'Heman',100),(29,'Batman',99),(30,'Jarvis',101);
+
+-- now we will learn about filtering commands such as IN, BETWEEN & LIKE OPERATORS
